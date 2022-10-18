c061e58807e7f14fb7f08553f332b0b9321c1645
agrad0 <110928774+agrad0@users.noreply.github.com>
2022-10-18 16:04:05 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let button = document.getElementById("get-data");
+
+const getData = () => {
+    fetch(`https://akademia108.pl/api/ajax/get-post.php`, {
+        mode: `cors`,
+        method: `GET`,
+    })
+    .then(response => response.json())
+    .then((data) => {
+        let userId = data.userId;
+        let id = data.id;
+        let importedBody = data.body;
+        let title = data.title;
+
+        
+        let idDescribe = document.createElement('p');
+        let userIdDescribe = document.createElement('p');
+        let titleDescribe = document.createElement('p');
+        let importedBodyDescribe = document.createElement('p');
+        let hr = document.createElement('hr');
+
+        idDescribe.innerText = `ID: ${id}`;
+        userIdDescribe.innerText = `User ID: ${userId}`;
+        titleDescribe.innerText = `Title: ${title}`;
+        importedBodyDescribe.innerText = `Body: ${importedBody}`;
+
+        document.body.appendChild(idDescribe);
+        document.body.appendChild(userIdDescribe);
+        document.body.appendChild(titleDescribe);
+        document.body.appendChild(importedBodyDescribe);
+        document.body.appendChild(hr);
+        (console.log(userId, id, importedBody, title))
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+}
+
+button.addEventListener("click", getData)
